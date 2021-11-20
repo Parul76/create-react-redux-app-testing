49ce38afb435f24bbfa8ba468586eb7068114995
Parul <parul@example.com>
2021-11-20 13:59:42 +0100

    add react testing

--- src/components/dresses/Dresscoll.js
+++ src/components/dresses/Dresscoll.js
@@ -1,11 +1,12 @@
 import React from "react";
-
+// import img from "../img.png";
 const DressesCollection = () => (
   <div>
     <h2>Dresses</h2>
     <p>
       We are sellings of new style of girl dresses collection with interesting
       designs!!!
+      {/* <img src={img} width="480" height="250" alt="Min cake" /> */}
     </p>
   </div>
 );
